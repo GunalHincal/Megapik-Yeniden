3483f9caadd7799ec64d37268bcf472450b0f19f
Gunal Hincal <hincalgunal@gmail.com>
2025-05-04 04:57:36 +0300

    Update requirements.txt: include only directly imported dependencies

--- vector_store_api.py
+++ vector_store_api.py
@@ -6,9 +6,8 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyMuPDFLoader
-from typing import List
 import os
-import shutil
+
 
 try:
     from langchain_huggingface import HuggingFaceEmbeddings
